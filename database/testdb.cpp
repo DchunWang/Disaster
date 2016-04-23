84f097135b37d429a03b5cd53608f03bcb4059d0
StarGazer <wangdchun@qq.com>
2016-04-23 18:50:20 +0800

    要怎样才能正确查询数据库数据呀？？？
    现在完全被搞懵了，什么鬼？

--- database/testdb.cpp
+++ database/testdb.cpp
@@ -14,7 +14,7 @@ void TestDB::initTest()
 {
 
     //采用MySQl数据库，建立一个数据库连接对象？？？
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
 
     /** 问题一：
      * “数据库”与“数据库连接”有什么区别？？？
@@ -22,16 +22,17 @@ void TestDB::initTest()
 
     /****************************设置,这些设置都起什么作用？？？*****************/
     //设置数据库主机名
-//    db.setHostName("localhost");
-//    //设置端口号
-//    db.setPort(8080);
-//    //设置数据库登录用户名
-//    db.setUserName("root");
-//    //设置数据库登录密码
-//    db.setPassword("47592kbtd@#Google");
-//    //设置数据库名
-//    db.setDatabaseName("DB_name");
+    db.setHostName("localhost");
+    //设置端口号
+    //db.setPort(8080);
+    //设置数据库登录用户名
+    db.setUserName("root");
+    //设置数据库登录密码
+    db.setPassword("47592kbtd@#Google");
+    //设置数据库名
+    db.setDatabaseName("index_test");
 
+    db.open();
     //打开数据库
     bool openDB = db.open();
 
@@ -46,24 +47,55 @@ void TestDB::initTest()
     //创建数据库后，再创建数据库表，并进行插入数据
     QSqlQuery query;
     //新建数据库表
-    query.exec("create table disa(id INTEGER,"
-               "name nvarchar(20),"
-               "hw INTEGER");
+    query.exec("create table disa(id INT PRIMARY KEY UNIQUE AUTO_INCREMENT,"
+               "name VARCHAR(20),"
+               "hw INT");
     //向表中插入数据
-    query.exec("insert into disa(88,'shadi',55)");
-    query.exec("insert into disa(77, 'tiandi', 22)");
-
+    query.exec("insert into disa(name, hw) VALUE('shadi',55)");
+    query.exec("insert into disa(name, hw) VALUE('tiandi', 22)");
+    query.exec("insert into disa(name, hw) VLAUE('wang', 33)");
+    query.exec("insert into disa(name, hw) VALUE('dechun', 44)");
+    if(query.isActive())
+    {
+        QMessageBox::information(0, tr("执行情况"), tr("执行了语句"));
+    }
 
     //查询数据库表中的数据
-    //QSqlQuery queryS;
-    query.exec("select soilType from disa");
+//    //QSqlQuery queryS;
+//    query.exec("select name from disa");
+//    //query.first();
+//    QString tempStr = "T";
 
-    int qsss = query.value(1).toInt();
-    QString qsi = query.value(2).toString();
-    //QString teStr = query.first().value(2).toString();
+//    while(query.next())
+//    {
+//        //int qsss = query.value(1).toInt();
+//        QString qsi = query.value(0).toString();
+//        //QString teStr = query.first().value(2).toString();
+//        tempStr += "+" + qsi;
+//    }
 
-    QMessageBox::information(0, tr("查询数据库表数据"), qsi);
+//    //int qsss = query.value(1).toInt();
+//    //QString qsi = query.value(0).toString();
+//    //QString teStr = query.first().value(2).toString();
+//    QMessageBox::information(0, tr("查询数据库表数据"), tempStr);
 
+
+
+
+
+
+
+
+
+    //返回所有可用的数据库驱动清单
+    QStringList DB_driverList = QSqlDatabase::drivers();
+    //将上述的数据库驱动清单显示出来
+    QString totalDriver;
+    foreach(QString dStr, DB_driverList)
+    {
+        totalDriver += dStr + "--";
+    }
+    //QMessageBox::information(0, tr("所有可用的数据库驱动"), totalDriver);
 
 
 
